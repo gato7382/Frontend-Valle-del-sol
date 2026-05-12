3aa5e7dc2c25be371a3c50a992b17adc950376dd
Joshua <josh.martinezs@duocuc.cl>
2026-05-12 19:49:59 -0400

    Error arreglado de conexiones

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Inicio from './components/page/inicio.jsx';
 import LoginLayout from './components/layout/LoginLayout.jsx';
 import Register from './components/layout/RegistroLayout.jsx';
 import Reportes from './components/page/Reportes.jsx';
+import Alertas from './components/page/Alertas.jsx';
 import Mapa from './components/page/Mapa.jsx';
 
-// Aquí es donde definiremos qué se ve en las otras pestañas
 function App() {
   return (
     <Router>
@@ -14,14 +14,11 @@ function App() {
         <Route path="/login" element={<LoginLayout />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />}>
-          {/* Esta es la vista que ya tienes (Home) */}
           <Route index element={<Inicio />} />
-          {/* ACTIVAMOS las rutas. Por ahora les pondremos un texto para que no den error */}
           <Route path="reportes" element={<Reportes />} />
           <Route path="mapa" element={<Mapa />} />
-          <Route path="alertas" element={<div style={{padding: '40px'}}><h2>🛎️ Panel de Alertas</h2><p>Listado de alarmas activas.</p></div>} />
+          <Route path="alertas" element={<Alertas />} />
           <Route path="bomberos" element={<div style={{padding: '40px'}}><h2>🧯 Personal de Bomberos</h2><p>Gestión de voluntarios.</p></div>} />
-          
         </Route>
       </Routes>
     </Router>
